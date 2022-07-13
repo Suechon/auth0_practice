17c07bdef0d0a9477f0e28e71cdc55545d34997e
Suechon <ss.710@i.softbank.jp>
2022-07-14 02:51:11 +0900

    api側の実装

---
+++ api/src/main/java/com/auth0/example/Application.java
@@ -0,0 +1,14 @@
+package com.auth0.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+
+	}
+
+}
